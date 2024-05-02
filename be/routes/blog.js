50cfaef82c1e79fc0a94dc676c78cbc1fce2311b
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2024-05-02 21:44:19 +0100

    blog API  routes

--- be/routes/blog.js
+++ be/routes/blog.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { CreatePost, GetAllPosts } = require("../controller/blog");
+const {
+  CreatePost,
+  GetAllPosts,
+  DeletePost,
+  UpdatePost,
+  GetAPost,
+  GetAllPostsByUserId,
+} = require("../controller/blog");
 
 router.get("/r", (req, res) => {
   console.log("test");
@@ -11,5 +18,9 @@ router.get("/r", (req, res) => {
 
 router.post("/", CreatePost);
 router.get("/", GetAllPosts);
+router.get("/user", authMiddleware, GetAllPostsByUserId);
+router.get("/:postId", GetAPost);
+router.put("/:postId", authMiddleware, UpdatePost);
+router.delete("/:postId", authMiddleware, DeletePost);
 
 module.exports = router;
